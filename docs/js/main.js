8aa2b5015eef0ce6c84937d8a14bbbee06412ac5
Erik Trefzger <mbairm3@eriks-air.lan>
2025-01-26 09:47:14 -0800

    update8

---
+++ docs/js/main.js
@@ -0,0 +1,5 @@
+// Add mobile menu functionality
+document.addEventListener('DOMContentLoaded', () => {
+    // Future enhancements can be added here
+    console.log('Site loaded successfully');
+}); 
